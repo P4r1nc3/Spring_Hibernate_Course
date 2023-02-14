11ee24464878a48a7e6d19b95ab7434b56cc12fb
parince <konrad.tupta@gmail.com>
2023-02-14 12:31:55 +0100

    Beans without XML

--- spring-demo-annotations/spring-demo-annotations/src/com.springdemo/SportConfig.java
+++ spring-demo-annotations/spring-demo-annotations/src/com.springdemo/SportConfig.java
@@ -1,10 +1,21 @@
 package com.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.springdemo")
+// @ComponentScan("com.springdemo")
 public class SportConfig {
 
+    // define bean for sad fortune service
+    @Bean
+    public FortuneService sadFortuneService () {
+        return new SadFortuneService();
+    }
+    // define bean for swim coach and inject dependency
+    @Bean
+    public Coach swimCoach () {
+        return new SwimCoach(sadFortuneService());
+    }
 }
