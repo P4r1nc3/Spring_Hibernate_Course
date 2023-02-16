1df6c0c5a1648c94e2f3f1e2eef80736a2fde139
parince <konrad.tupta@gmail.com>
2023-02-16 10:48:01 +0100

    Request param binding

--- spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -2,7 +2,11 @@
 <html>
 
 	<body>
-		<h2> Spring MVC</h2>
+		<h2> Spring MVC - Main Menu </h2>
+		
+		<br>
+		
+		<a href="showForm">Hello World Form</a>
 	</body>
 
 </html>
---
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,55 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String processForm(HttpServletRequest request, Model model) {
+		
+		// get name from HTML form
+		String theName = request.getParameter("studentName");
+		
+		// make name upper case
+		theName = theName.toUpperCase();
+		
+		// create the message 
+		String result = "Yo " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String rocessFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+		
+		// make name upper case
+		theName = theName.toUpperCase();
+		
+		// create the message 
+		String result = "Hello " + theName + "!";
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+}
