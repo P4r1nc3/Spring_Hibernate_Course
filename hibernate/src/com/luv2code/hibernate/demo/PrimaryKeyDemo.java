0b2603f9f9deb3d38d7f8c725924e8647f7c213c
parince <konrad.tupta@gmail.com>
2023-02-18 14:10:00 +0100

    Hibernate primary keys

--- hibernate/src/com/luv2code/hibernate/demo/PrimaryKeyDemo.java
+++ hibernate/src/com/luv2code/hibernate/demo/PrimaryKeyDemo.java
@@ -1,16 +1,15 @@
 package com.luv2code.hibernate.demo;
 
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import com.luv2code.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class PrimaryKeyDemo {
 
 	public static void main(String[] args) {
-		
+
 		// create session factory
 		SessionFactory factory = new Configuration()
 									.configure("hibernate.cfg.xml")
@@ -22,17 +21,21 @@ public class CreateStudentDemo {
 		
 		try {
 			// use the session object
-			System.out.println("Creating a new student object...");
+			System.out.println("Creating a new student objects...");
 			
-			// create student object 
-			Student tempStudent = new Student("Paul", "Wall", "paulwall@example.com");
+			// create 3 students object 
+			Student tempStudent1 = new Student("John", "Doe", "paulwall@example.com");
+			Student tempStudent2 = new Student("Paul", "Walker", "paulwalker@example.com");
+			Student tempStudent3 = new Student("John", "Deer", "johndeer@example.com");
 			
 			// start a transaction
 			session.beginTransaction();
 			
-			// save a student object 
+			// save a students objects
 			System.out.println("Saving a new student...");
-			session.save(tempStudent);
+			session.save(tempStudent1);
+			session.save(tempStudent2);
+			session.save(tempStudent3);
 			
 			// commit transaction
 			session.getTransaction().commit();
@@ -42,5 +45,9 @@ public class CreateStudentDemo {
 		finally {
 			factory.close();
 		}
+		
+		
+
 	}
+
 }
