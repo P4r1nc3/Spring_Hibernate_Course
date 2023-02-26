91f819060027ee8b6265c6034e6d51a25ed94c69
parince <konrad.tupta@gmail.com>
2023-02-26 11:04:23 +0100

    Fixed packages error

--- web-customer-tracker/src/com/luv2code/springdemo/entity/Customer.java
+++ web-customer-tracker/src/com/luv2code/springdemo/entity/Customer.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="custmer")
+@Table(name="customer")
 public class Customer {
 
 	@Id
@@ -64,5 +64,9 @@ public class Customer {
 	@Override
 	public String toString() {
 		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
-	}
+	}	
 }
+
+
+
+
