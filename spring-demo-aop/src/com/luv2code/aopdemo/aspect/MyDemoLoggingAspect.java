3e393734517eb7260d3b65739cc9ff58a438dc5b
parince <konrad.tupta@gmail.com>
2023-02-28 12:36:36 +0100

    Spring AOP: match any addAccount method

--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,23 +9,13 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
 	// this is where we add all of our related advices for logging
-	
+
 	// let's start with an @Before advice
 
 	@Before("execution(public void addAccount())")
 	public void beforeAddAccountAdvice() {
-		
-		System.out.println("\n=====>>> Executing @Before advice on addAccount()");
-		
+
+		System.out.println("\n=====>>> Executing @Before advice on method");
+
 	}
 }
-
-
-
-
-
-
-
-
-
-
